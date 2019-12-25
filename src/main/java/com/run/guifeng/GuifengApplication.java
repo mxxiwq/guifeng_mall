a850b83842f2275fa93b5d45811bf5f709cf6c71
JingShi <chenyangwork1@163.com>
2019-12-25 09:48:37 +0800

    Initial commit

---
+++ src/main/java/com/run/guifeng/GuifengApplication.java
@@ -0,0 +1,13 @@
+package com.run.guifeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuifengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuifengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/run/guifeng/GuifengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.run.guifeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuifengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
